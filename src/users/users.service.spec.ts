6e037efdb254c1bc16dfe0ae00abfe7ff2c92669
Lucas Cabral <lucassimple@hotmail.com>
2025-11-30 00:20:47 -0300

    tercer intento de prueba de github actions

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,10 +7,10 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
-    }).compile();
+    }).compile()
 
     service = module.get<UsersService>(UsersService);
-  });
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
