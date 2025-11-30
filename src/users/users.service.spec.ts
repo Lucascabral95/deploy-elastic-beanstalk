c3cda7df7fe952bcae4173e6a83652668a8d77ca
Lucas Cabral <lucassimple@hotmail.com>
2025-11-30 00:22:47 -0300

    cuarto intento de hacer funcionar github actions

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: UsersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
